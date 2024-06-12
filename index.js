d03688ea3fc979f93de8ae66e1ed6679b6dcf651
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-12 19:36:31 -0300

    feat: pergunta para o usuario

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-let nome = 'Fabio', console.log('nome')
+let resposta
+
+console.log('Você toca algum instrumento?');
+
+process.stdin.on('data', function (data){
+    resposta = data.toString().trim();
+
+    if (resposta == 'Sim' || resposta == 'sim' || resposta == 's') {
+        console.log('Que legal.');
+    } else if (resposta == 'Não' || resposta == 'nao' || resposta == 'n') {
+        console.log('Que pena. Tocar um instrumento é muito legal.');
+    } else {
+        console.log('Não entendi. Pode repetir?');
+    }
+    process.exit();
+})
+
